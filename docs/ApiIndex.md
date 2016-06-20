44e7e7c7b4ae6d6f4db35fa61b7c912facf18895
Amy Chen <amchen@adobe.com>
2016-06-20 14:50:12 -0400

    akjshd

--- docs/ApiIndex.md
+++
@@ -1,8 +0,0 @@
-API Index
-=========
-
-* Behance
-    * Behance\NBD
-        * Behance\NBD\Gatekeeper
-            * [Gatekeeper](Behance-NBD-Gatekeeper-Gatekeeper.md)
-
--- vendor/behance/php-sniffs/tests/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once( __DIR__ . '/../vendor/autoload.php' );
-define( 'TEST_PATH', __DIR__ . '/Behance' );
-define( 'STANDARD_PATH', __DIR__ . '/../Behance' );
-define( 'TEST_EXT', 'SniffTest' );
